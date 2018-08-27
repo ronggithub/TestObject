8b91713db64c47e72a4cb66bf9a3c8d990d5b867
zhangzhenrong <v_zhangzhenrong@baidu.com>
2018-08-27 17:55:14 +0800

    rm other

--- app/src/main/java/com/baidu/searchbox/MainActivity.java
+++ app/src/main/java/com/baidu/searchbox/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_main);
 
         textChannel = findViewById(R.id.channel);
-        textChannel.setText("OPPO channel:" + getOPPOChannel("com.baidu.searchbox.channel"));
+        textChannel.setText("");
 
         button = findViewById(R.id.main_button);
         apptargetbt = findViewById(R.id.apptargetbt);
@@ -44,54 +44,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         apptargetbt.setOnClickListener(this);
     }
 
-    /**
-     * 获取OPPO预置目录下渠道号，优先读取data目录，如读取不到再读取system目录
-     * @param fileName 文件名
-     * @return OPPO渠道号
-     */
-    public String getOPPOChannel(String fileName) {
-        String channel = "";
-        String oppoDataPath = "/data/etc/appchannel/" + fileName;
-        String oppoSystemPath = "/system/etc/appchannel/" + fileName;
-        File file = new File(oppoDataPath);
-        InputStream inputStream = null;
-        BufferedReader reader = null;
-        try {
-            if (file.exists() && file.canRead()) {
-                inputStream = new FileInputStream(file);
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-                channel = reader.readLine();
-                if (DEBUG) {
-                    android.util.Log.d(TAG, "OPPO read data channel success & channel is : " + channel);
-                }
-            } else {
-                file = new File(oppoSystemPath);
-                if (file.exists() && file.canRead()) {
-                    inputStream = new FileInputStream(file);
-                    reader = new BufferedReader(new InputStreamReader(inputStream));
-                    channel = reader.readLine();
-                    if (DEBUG) {
-                        android.util.Log.d(TAG, "OPPO read system channel success & channel is : " + channel);
-                    }
-                }
-            }
-        } catch (IOException e) {
-            if (DEBUG) {
-                android.util.Log.d(TAG, "OPPO read channel error");
-            }
-            e.printStackTrace();
-        } finally {
-            try {
-                if (inputStream != null && reader != null) {
-                    inputStream.close();
-                    reader.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return channel;
-    }
+
 
 
     @Override
